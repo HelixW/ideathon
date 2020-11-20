00c914ead5ce9d46a11c1969d49c4989cc2a450e
HelixW <shreyas.2000@hotmail.com>
2020-11-20 16:44:12 +0530

    feat: landing responsive
    Signed-off-by: HelixW <shreyas.2000@hotmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false,
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1245px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     backgroundColor: (theme) => ({
       ...theme("colors"),
       "idea-blue": "#2D9CDB",
