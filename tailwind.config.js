58d3b36f3426875e75dc6ca25075a70522f7b802
HelixW <shreyas.2000@hotmail.com>
2020-11-20 18:02:14 +0530

    fix: sub text fix
    Signed-off-by: HelixW <shreyas.2000@hotmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,13 @@
 module.exports = {
-  purge: [],
+  purge: { enabled: true, content: ["./src/**/*.html"] },
   darkMode: false,
   theme: {
     screens: {
       sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
       md: "768px",
-      // => @media (min-width: 768px) { ... }
-
       lg: "1245px",
-      // => @media (min-width: 1024px) { ... }
-
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
       "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
     },
     backgroundColor: (theme) => ({
       ...theme("colors"),
