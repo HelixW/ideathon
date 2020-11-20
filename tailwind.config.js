f7256e086210932289a096f924dce9838d9761dc
HelixW <shreyas.2000@hotmail.com>
2020-11-20 16:19:39 +0530

    feat: add landing page
    Signed-off-by: HelixW <shreyas.2000@hotmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false,
   theme: {
-    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      "idea-blue": "#2D9CDB",
+    }),
+    textColor: (theme) => ({
+      ...theme("colors"),
+      "idea-pink": "#EB5757",
+      "idea-gray": "#4F4F4F",
+    }),
   },
   variants: {
     extend: {},
